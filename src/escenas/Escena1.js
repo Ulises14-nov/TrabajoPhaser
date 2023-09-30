d4449639ae858ba2183c607ca73de439389c3430
Ulises14-nov <toconasdanielulises@gmail.com>
2023-09-30 12:38:54 -0300

    se sube la primera parte del proyecto con las escena 1 completa

---
+++ src/escenas/Escena1.js
@@ -0,0 +1,134 @@
+class Escena1 extends Phaser.Scene{
+   
+    constructor(){
+        super("Escena1");
+        this.platforms=null;
+        this.score = 0;
+        this.gameOver = false;
+    }
+
+    preload(){
+        this.load.image('sky', '../public/img/sky.png');
+        this.load.image('ground', '../public/img/platform.png');
+        this.load.image('star', '../public/img/star.png');
+        this.load.image('bomb', '../public/img/bomb.png');
+        this.load.spritesheet('dude','../public/img/dude.png',{ frameWidth: 32, frameHeight: 48 });
+    }
+    
+    create(){
+        //----barraas
+        this.add.image(400, 300, 'sky');
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');    
+
+         this.add.image(400, 300, 'star');
+        // //jugador
+         this.player = this.physics.add.sprite(100, 450, 'dude');
+         this.player.setBounce(0.2);
+         this.player.setCollideWorldBounds(true);
+        
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+        
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+         this.player.body.setGravityY(300);//
+         this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        });
+
+        this.stars.children.iterate(function (child) {
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        });
+
+        this.bombs = this.physics.add.group();
+        this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+        this.physics.add.collider(player, platforms);
+        this.physics.add.collider(stars, platforms);
+        this.physics.add.collider(bombs, platforms);
+
+        this.physics.add.overlap(player, stars, collectStar, null, this);
+        this.physics.add.collider(player, bombs, hitBomb, null, this);
+    }
+ 
+    //update(time, deltatime){
+    update(){
+        if (gameOver)
+        {
+            return;
+        }
+
+        if (cursors.left.isDown)
+            {
+                player.setVelocityX(-160);
+                player.anims.play('left', true);
+            }
+            else if (cursors.right.isDown)
+            {
+                player.setVelocityX(160);
+                player.anims.play('right', true);
+            }
+            else
+            {
+                player.setVelocityX(0);
+                player.anims.play('turn');
+            }
+            if (cursors.up.isDown && player.body.touching.down)
+            {
+                player.setVelocityY(-330);
+            }
+    } 
+    collectStar (player, star)
+    {
+        star.disableBody(true, true);
+        this.score += 10;
+        this.scoreText.setText('Score: ' + score);
+
+        if (stars.countActive(true) === 0)
+        {
+            stars.children.iterate(function (child) {
+                child.enableBody(true, child.x, 0, true, true);
+            });
+
+            let x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+            let bomb = bombs.create(x, 16, 'bomb');
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+            bomb.allowGravity = false;
+        }
+    }
+
+    hitBomb (player, bomb)
+    {
+        this.physics.pause();
+        player.setTint(0xff0000);
+        player.anims.play('turn');
+        gameOver = true;
+    }
+
+}
+export default Escena1;
